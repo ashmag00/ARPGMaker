07a192d71496625d7a708d380274f7baf2f02922
nlindsey <noah_lindsey@taylor.edu>
2019-04-05 12:54:34 -0400

    Update ext.cpp

--- ext.cpp
+++ ext.cpp
@@ -18,17 +18,16 @@ static PyObject *spam_system(PyObject *self, PyObject *args) {
 }
 
 static PyMethodDef SpamMethods[] = {
-    {"system",  spam_system, METH_VARARGS,
-     "Execute a shell command."},
+    {"system",  spam_system, METH_VARARGS, "Execute a shell command."},
     {NULL, NULL, 0, NULL}
 };
 
 static struct PyModuleDef spammodule = {
     PyModuleDef_HEAD_INIT,
-    "spam",   /* name of module */
-    NULL, /* module documentation, may be NULL */
-    -1,       /* size of per-interpreter state of the module,
-                 or -1 if the module keeps state in global variables. */
+    "spam",
+    NULL,
+    -1,
+
     SpamMethods
 };
 
@@ -37,26 +36,3 @@ PyInit_spam(void)
 {
     return PyModule_Create(&spammodule);
 }
-
-// static PyObject *ext_printHello(PyObject *self, PyObject *args) {
-//     if (!PyArg_ParseTuple(args, "s", NULL)) return NULL;
-//     return Py_BuildValue("s", printHello(void));
-// }
-
-// static PyMethodDef extMethods[] = {
-//     {"ext_printHello", (PyCFunction)ext_printHello, METH_VARARGS},
-//     {NULL, NULL, 0, NULL}
-// };
-
-// static struct PyModuleDef hellomodule = {
-//     PyModuleDef_HEAD_INIT,
-//     "hello",
-//     NULL,
-//     -1,
-
-//     extMethods
-// };
-
-// PyMODINIT_FUNC initest() {
-//     return PyModule_Create(&hellomodule);
-// }
