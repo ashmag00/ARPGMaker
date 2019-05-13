427a998b01efb8bc10e32443a1967a0717699fda
gabe_helmuth@taylor.edu <ghelmuth@cse21706.cs.local>
2019-05-13 18:11:15 -0400

    finished Sound.cpp and Sound.h capability to plug multiple sounds into a single buffer from an anchor sound

--- engine/Sound.h
+++ engine/Sound.h
@@ -7,38 +7,44 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include "Map.h"
+#include "memory.h"
+
+extern unsigned int currentID;
 
 class Sound {
     public:
         Sound();
         void setFilePath(char *fileName);
         int loadSoundFile(char *fileName);
-        //void setBuffer(sf::SoundBuffer buffer);
+        void setBuffer(sf::SoundBuffer *buffer);
         void playSound();
         void pauseSound();
         void stopSound();
         void setLoop(int setting);
         void setVolume(unsigned int vol);
+        int getID();
+        sf::SoundBuffer* getBuffer();
         int loadedBuffer();
 
         // Used for DEBUGGING
         int main();
 
+        int id;
         char *filePath;
-        sf::SoundBuffer buffer;
+        sf::SoundBuffer *buffer = new sf::SoundBuffer;
         sf::Sound sound;
         bool loadedBuff;
 };
 
-char * createSound(char *fileName, int giveBuffer);
-//void setBuffer(char *filePath);
-void playSound(char *filePath);
-void pauseSound(char *filePath);
-void stopSound(char *filePath);
-void setLoop(char *filePath, int setting);
-void setVolume(char *filePath, unsigned int vol);
-void accessBuffer(char *filePath);
-Sound* getSoundByFilePath(char *filePath);
+int createSound(char *fileName, int giveBuffer);
+void setBuffer(int ID, int bufferID);
+void playSound(int ID);
+void pauseSound(int ID);
+void stopSound(int ID);
+void setLoop(int ID, int setting);
+void setVolume(int ID, unsigned int vol);
+void accessBuffer(int ID);
+Sound* getSoundByID(int ID);
 //SoundBuffer* getBufferByFilePath(char *filePath);
 
 
