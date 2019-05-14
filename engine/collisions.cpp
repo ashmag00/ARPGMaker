cdb6b4b6f5ea72e4644ac24ec10f3227d7aafe5b
asher_gingerich@taylor.edu <agingeri@cse21704.cs.local>
2019-05-14 17:53:19 -0400

    Correct collision math

--- engine/collisions.cpp
+++ engine/collisions.cpp
@@ -1,10 +1,16 @@
 #include "collisions.h"
 
 extern Map demoMap;
+extern std::unordered_map<char *, sf::Texture> textureHash;
 
 bool circleCollide(Entity *ent1, Entity *ent2) {
     // sqrt((x2-x1)^2 + (y2-y1)^2)
-    float distance = sqrt(pow(ent2->x - ent1->x, 2) + pow(ent2->y - ent1->y,2));
+    sf::Texture tex1 = textureHash[ent1->texture];
+    sf::Texture tex2 = textureHash[ent2->texture];
+    sf::Vector2u vec1 = tex1.getSize();
+    sf::Vector2u vec2 = tex2.getSize();
+    float distance = sqrt(pow(ent2->x + (vec2.x / 2) - (
+        ent1->x + (vec1.y / 2)),2) + pow(ent2->y + (vec2.y / 2) - (ent1->y + (vec1.y / 2)),2));
     return distance < (ent1->radius + ent2->radius);
 }
 
